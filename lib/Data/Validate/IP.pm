6f1de6132b49b0245e25bfa5b171c5deeda834e0
Dave Rolsky <autarch@urth.org>
2013-02-18 23:13:05 -0600

    Add support for all forms of network allowed by Net::Netmask

--- lib/Data/Validate/IP.pm
+++ lib/Data/Validate/IP.pm
@@ -371,29 +371,8 @@ The potential ip to test.
 
 =item $network
 
-The potential network the IP must be a part of. Functionality uses Net::Netmask and should be in the form:
-
-       '216.240.32.0/24'               The preferred form.
-
-       '216.240.32.0:255.255.255.0'
-       '216.240.32.0-255.255.255.0'
-       '216.240.32.0 - 216.240.32.255'
-       '216.240.32.4'                  A /32 block.
-
-       '216.240.32'                    Always a /24 block.
-
-       '216.240'                       Always a /16 block.
-
-       '140'                           Always a /8 block.
-
-       '216.240.32/24'
-       '216.240/16'
-       'default'                       0.0.0.0/0 (the default route)
-
-       '216.240.32.0#0.0.31.255'       A hostmask (as used by Cisco
-                                       access-lists).
-
-Examples taken from Net::Netmask documentation.  For more advanced network matching needs please see Net::Netmask.
+The potential network the IP must be a part of. This should be specified in
+CIDR notation, for example "15.0.15.0/24".
 
 =back
 
@@ -410,6 +389,10 @@ actually exists.
 
 =cut
 
+# This is just a quick test - we'll let NetAddr::IP decide if the address is
+# valid.
+my $ip_re = qr/\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/;
+my $partial_ip_re = qr/\d{1,3}(?:\.\d{1,3}){0,2}/;
 sub is_innet_ipv4 {
     shift if ref $_[0];
     my $value   = shift;
@@ -420,11 +403,133 @@ sub is_innet_ipv4 {
     my $ip = is_ipv4($value);
     return unless defined $ip;
 
+    # Backwards compatibility hacks to make it accept things that Net::Netmask
+    # accepts.
+    unless ($network eq 'default'
+        || $network =~ /^$ip_re$/
+        || $network =~ m{^$ip_re/\d\d?$}) {
+
+        if ($network =~ /^($ip_re)[:\-]($ip_re)$/) {
+            my ($net, $netmask) = ($1, $2);
+
+            my $bits = _netmask_to_bits($netmask)
+                or return;
+
+            $network = "$net/$bits";
+        }
+        elsif ($network =~ /^($ip_re)\#($ip_re)$/) {
+            my ($net, $hostmask) = ($1, $2);
+
+            my $bits = _hostmask_to_bits($hostmask)
+                or return;
+
+            $network = "$net/$bits";
+        }
+        elsif ($network =~ m{^($partial_ip_re)/(\d\d?)$}) {
+            my ($net, $bits) = ($1, $2);
+
+            my $octets = scalar split /\./, $net;
+            $network = $net;
+            $network .= '.0' x (4 - $octets);
+            $network .= "/$bits";
+        }
+        elsif ($network =~ /^$partial_ip_re$/) {
+            my $octets = scalar split /\./, $network;
+            if ($octets < 4) {
+                $network .= '.0' x (4 - $octets);
+                $network .= '/' . $octets * 8;
+            }
+        }
+    }
+
     $network = NetAddr::IP->new($network) or return;
     my $netaddr_ip = NetAddr::IP->new($ip) or return;
 
     return $ip if $network->contains($netaddr_ip);
     return;
+}
+
+{
+    my %netmasks = (
+        '128.0.0.0'       => '1',
+        '192.0.0.0'       => '2',
+        '224.0.0.0'       => '3',
+        '240.0.0.0'       => '4',
+        '248.0.0.0'       => '5',
+        '252.0.0.0'       => '6',
+        '254.0.0.0'       => '7',
+        '255.0.0.0'       => '8',
+        '255.128.0.0'     => '9',
+        '255.192.0.0'     => '10',
+        '255.224.0.0'     => '11',
+        '255.240.0.0'     => '12',
+        '255.248.0.0'     => '13',
+        '255.252.0.0'     => '14',
+        '255.254.0.0'     => '15',
+        '255.255.0.0'     => '16',
+        '255.255.128.0'   => '17',
+        '255.255.192.0'   => '18',
+        '255.255.224.0'   => '19',
+        '255.255.240.0'   => '20',
+        '255.255.248.0'   => '21',
+        '255.255.252.0'   => '22',
+        '255.255.254.0'   => '23',
+        '255.255.255.0'   => '24',
+        '255.255.255.128' => '25',
+        '255.255.255.192' => '26',
+        '255.255.255.224' => '27',
+        '255.255.255.240' => '28',
+        '255.255.255.248' => '29',
+        '255.255.255.252' => '30',
+        '255.255.255.254' => '31',
+        '255.255.255.255' => '32',
+    );
+
+    sub _netmask_to_bits {
+        return $netmasks{$_[0]};
+    }
+}
+
+{
+    my %hostmasks = (
+        '255.255.255.255' => 0,
+        '127.255.255.255' => 1,
+        '63.255.255.255'  => 2,
+        '31.255.255.255'  => 3,
+        '15.255.255.255'  => 4,
+        '7.255.255.255'   => 5,
+        '3.255.255.255'   => 6,
+        '1.255.255.255'   => 7,
+        '0.255.255.255'   => 8,
+        '0.127.255.255'   => 9,
+        '0.63.255.255'    => 10,
+        '0.31.255.255'    => 11,
+        '0.15.255.255'    => 12,
+        '0.7.255.255'     => 13,
+        '0.3.255.255'     => 14,
+        '0.1.255.255'     => 15,
+        '0.0.255.255'     => 16,
+        '0.0.127.255'     => 17,
+        '0.0.63.255'      => 18,
+        '0.0.31.255'      => 19,
+        '0.0.15.255'      => 20,
+        '0.0.7.255'       => 21,
+        '0.0.3.255'       => 22,
+        '0.0.1.255'       => 23,
+        '0.0.0.255'       => 24,
+        '0.0.0.127'       => 25,
+        '0.0.0.63'        => 26,
+        '0.0.0.31'        => 27,
+        '0.0.0.15'        => 28,
+        '0.0.0.7'         => 29,
+        '0.0.0.3'         => 30,
+        '0.0.0.1'         => 31,
+        '0.0.0.0'         => 32,
+    );
+
+    sub _hostmask_to_bits {
+        return $hostmasks{ $_[0] };
+    }
 }
 
 =pod
@@ -1113,8 +1218,6 @@ B<[RFC 2460] [RFC 4193] [RFC 4291] [RFC 6434]>
 =over 4
 
 =item  L<Data::Validate(3)>
-
-=item  L<Net::Netmask(3)>
 
 =back
 
