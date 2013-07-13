0fff3a52850527842e43697f50f9aacd6603c012
Dave Rolsky <autarch@urth.org>
2013-07-13 14:13:46 -0500

    Remove empty variables

--- lib/Data/Validate/IP.pm
+++ lib/Data/Validate/IP.pm
@@ -38,23 +38,6 @@ BEGIN {
 
 our @ISA = qw(Exporter);
 
-# Items to export into callers namespace by default. Note: do not export
-# names by default without a very good reason. Use EXPORT_OK instead.
-# Do not simply export all your public functions/methods/constants.
-
-# This allows declaration	use Data::Validate::IP ':all';
-# If you do not need this, moving things directly into @EXPORT or @EXPORT_OK
-# will save memory.
-our %EXPORT_TAGS = (
-    'all' => [
-        qw(
-
-            )
-    ]
-);
-
-our @EXPORT_OK = (@{ $EXPORT_TAGS{'all'} });
-
 our @EXPORT = qw(
     is_ipv4
     is_ipv6
